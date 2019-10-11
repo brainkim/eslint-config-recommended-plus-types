6746a735953bdd5ad280346d6b419136ecf68f81
Brian Kim <briankimpossible@gmail.com>
2019-10-11 01:39:32 -0400

    disable certain busted rules

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ module.exports = {
 	"plugins": ["@typescript-eslint"],
 	"rules": {
 		"@typescript-eslint/no-for-in-array": "error",
-		"@typescript-eslint/no-unnecessary-condition": "error",
+		// TODO: enable pending certain bugfixes
+		// https://github.com/typescript-eslint/typescript-eslint/issues/1067
+		//"@typescript-eslint/no-unnecessary-condition": "error",
 		"@typescript-eslint/restrict-plus-operands": "error",
 	},
 	"overrides": [
@@ -13,7 +15,10 @@ module.exports = {
 				"@typescript-eslint/adjacent-overload-signatures": "error",
 				"@typescript-eslint/no-misused-new": "error",
 				"@typescript-eslint/no-unnecessary-qualifier": "error",
-				"@typescript-eslint/no-unnecessary-type-assertion": "error",
+				// TODO: enable pending certain bugfixes
+				// https://github.com/typescript-eslint/typescript-eslint/issues/1061
+				// https://github.com/typescript-eslint/typescript-eslint/issues/1066
+				//"@typescript-eslint/no-unnecessary-type-assertion": "error",
 				"@typescript-eslint/no-unused-vars": "error",
 				"no-unused-vars": "off",
 			},
